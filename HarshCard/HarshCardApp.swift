fbb84625b5a6d1c4b3da0e46e831ec67fa0fe519
harshbsv <harshsolo.dev@gmail.com>
2021-04-29 13:02:17 +0530

    Initial Commit

---
+++ HarshCard/HarshCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarshCardApp.swift
+//  HarshCard
+//
+//  Created by Harshvardhan Basava on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HarshCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
